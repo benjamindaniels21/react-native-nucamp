32dbfad1e286402f290bdf9473ffb8f0142610eb
Ben Daniels <benjamindaniels21@gmail.com>
2021-11-18 07:46:56 -0500

    Workshop Task 1

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -59,7 +59,7 @@ function RenderCampsite(props) {
         image={{ uri: baseUrl + campsite.image }}
       >
         <Text style={{ margin: 10 }}>{campsite.description}</Text>
-        <View>
+        <View style={styles.cardRow}>
           <Icon
             name={props.favorite ? "heart" : "heart-o"}
             type="font-awesome"
@@ -78,7 +78,7 @@ function RenderCampsite(props) {
             color="#5637DD"
             raised
             reverse
-            onPress={() => props.showModal()}
+            onPress={() => props.onShowModal()}
           />
         </View>
       </Card>
@@ -122,11 +122,44 @@ class CampsiteInfo extends Component {
           campsite={campsite}
           favorite={this.props.favorites.includes(campsiteId)}
           markFavorite={() => this.markFavorite(campsiteId)}
+          onShowModal={() => this.toggleModal()}
         />
         <RenderComments comments={comments} />
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.showModal}
+          onRequestClose={() => this.toggleModal()}
+        >
+          <View style={styles.modal}>
+            <View style={{ margin: 10 }}>
+              <Button
+                onPress={() => {
+                  this.toggleModal();
+                }}
+                color="#808080"
+                title="Cancel"
+              />
+            </View>
+          </View>
+        </Modal>
       </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  cardRow: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row",
+    margin: 20,
+  },
+  modal: {
+    justifyContent: "center",
+    margin: 20,
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(CampsiteInfo);
