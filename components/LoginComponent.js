a16abe08677392e265ed5e8c28e97258a2f8c629
Ben Daniels <benjamindaniels21@gmail.com>
2021-12-04 11:20:05 -0500

    workshop task 2

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -176,6 +176,23 @@ class RegisterTab extends Component {
     this.setState({ imageUrl: processedImage.uri });
   };
 
+  getImageFromGallery = async () => {
+    const cameraRollPermissions = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL
+    );
+    if (cameraRollPermissions.status === "granted") {
+      const capturedImage = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [1, 1],
+      });
+      if (!capturedImage.cancelled) {
+        console.log(capturedImage);
+        // this.setState({ imageUrl: capturedImage.uri });
+        this.processImage(capturedImage.uri);
+      }
+    }
+  };
+
   handleRegister() {
     console.log(JSON.stringify(this.state));
     if (this.state.remember) {
@@ -204,6 +221,7 @@ class RegisterTab extends Component {
               style={styles.image}
             />
             <Button title="Camera" onPress={this.getImageFromCamera} />
+            <Button title="Gallery" onPress={this.getImageFromGallery} />
           </View>
           <Input
             placeholder="Username"
